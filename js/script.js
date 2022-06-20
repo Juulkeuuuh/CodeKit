6ec684f7176c45e98ab171db4c000ae2258f7983
Juulkeuuuh <90319980+Juulkeuuuh@users.noreply.github.com>
2022-06-20 22:19:55 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 
 $(document).ready(function(){
+    var count = 0;
 
+    $("#hamb-button").click(function(){
+        if (count == 0) {
+            $(".nav").css("display", "block");
+            count = 1;   
+        } else if (count == 1){
+            $(".nav").css("display", "none");
+            count = 0;
+        } 
+
+    })
 })
+
